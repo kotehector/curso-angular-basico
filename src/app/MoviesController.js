d60e4e5c8759b002617e03155023557f01c7ca3a
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-19 20:56:01 +0200

    ng-show / ng-hide

--- src/app/MoviesController.js
+++ src/app/MoviesController.js
@@ -1,16 +1,33 @@
 function MoviesController() {
   this.favorites = [{
     title: 'Matrix',
-    year: '1999'
+    year: '1999',
+    popular: true
   },{
     title: 'Salvando al Soldando Ryan',
-    year: '1998'
+    year: '1998',
+    popular: false
+  },{
+    title: 'Marte',
+    year: '2015',
+    popular: false
   },{
     title: 'El Padrino',
-    year: '1972'
+    year: '1972',
+    popular: true
   }];
 }
 
 angular
   .module('app')
   .controller('MoviesController', MoviesController);
+
+
+
+//* Lógica explicada con Toggle jQuery
+// var element = $('.shome-element');
+// if (condition) {
+//   element.show();
+// } else {
+//   element.hide();
+// }
