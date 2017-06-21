936aacdeb881d1d732e7602fedc69e895695d6f7
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-21 21:10:13 +0200

    filtrar y ordenar listas con ng-repeat

--- src/app/MainController.js
+++ src/app/MainController.js
@@ -1,5 +1,24 @@
 function MainController() {
-  this.myAmmount = 3480.75;
+  this.favorites = [{
+      title: 'Matrix',
+      year: '1999',
+      popular: true,
+      category: 'ciencia ficción'
+    },{
+      title: 'Cadena Perpetua',
+      year: '1994',
+      popular: false,
+      category: 'acción'
+    },{
+      title: 'Marte',
+      year: '2015',
+      popular: false
+    },{
+      title: 'El Padrino',
+      year: '1972',
+      popular: true,
+      category: 'mafia'
+    }];
 }
 
 angular
