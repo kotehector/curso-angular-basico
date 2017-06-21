93a1518d53407b74b14838bfa1ad4690e5704313
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-21 21:18:50 +0200

    json debug

--- src/app/MainController.js
+++ src/app/MainController.js
@@ -1,24 +1,8 @@
 function MainController() {
-  this.favorites = [{
-      title: 'Matrix',
-      year: '1999',
-      popular: true,
-      category: 'ciencia ficción'
-    },{
-      title: 'Cadena Perpetua',
-      year: '1994',
-      popular: false,
-      category: 'acción'
-    },{
-      title: 'Marte',
-      year: '2015',
-      popular: false
-    },{
-      title: 'El Padrino',
-      year: '1972',
-      popular: true,
-      category: 'mafia'
-    }];
+  this.person = {
+    name: '',
+    city: ''
+  }
 }
 
 angular
