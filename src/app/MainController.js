e4475bc23e53e27e749c26f25019722c373bd00b
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-23 13:14:18 +0200

    $http y unir la respuesta a la Vista

--- src/app/MainController.js
+++ src/app/MainController.js
@@ -1,7 +1,19 @@
-function MainController() {
-  this.person = {
-    name: '',
-    city: ''
+function MainController($http) {
+  var ctrl = this;
+  var API = '//jsonplaceholder.typicode.com/users/';
+  this.userId = '';
+  this.chosenUser = {};
+  this.getUser = function() {
+    if(!this.userId) {
+      return;
+    }
+    $http
+      .get(API + this.userId)
+      .then(function(response) {
+        ctrl.chosenUser = response.data;
+      }, function(response) {
+        console.log(response);
+      })
   }
 }
 
