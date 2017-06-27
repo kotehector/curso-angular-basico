6468a571e711ecfb76f6577396368f1039450e07
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-27 21:18:03 +0200

    Validaciones internas de AngularJs y Estados de inputs en el formulario

--- src/app/order-form.directive.js
+++ src/app/order-form.directive.js
@@ -8,15 +8,27 @@ function orderForm() {
     restrict: 'E',
     controller: 'OrderFormController as form',
     template: `
+      <pre>{{ orderForm | json }}</pre>
       <pre>{{ form.data | json }}</pre>
-      <form novalidate ng-submit="form.onSubmit();">
-        <input type='text' placeholder='Nombre' ng-model="form.data.name">
-        <input type='email' placeholder='Email' ng-model="form.data.email">
-        <input type='text' placeholder='Ciudad' ng-model="form.data.location">
+      <form name="orderForm" novalidate ng-submit="form.onSubmit();">
+        <input name="name"
+               type='text'
+               placeholder='Nombre'
+               ng-model="form.data.name"
+               required=''>
+        <input name="email"
+               type='email'
+               placeholder='Email'
+               ng-model="form.data.email"
+               required=''>
+        <input name='location'
+               placeholder='Ciudad'
+               ng-model="form.data.location"
+               required=''>
         <select>
           <option>Tu ciudad</option>
         </select>
-        <textarea placeholder='Tu mensaje (opcional)' ng-model="form.data.location"></textarea>
+        <textarea name="comments" placeholder='Tu mensaje (opcional)' ng-model="form.data.comments"></textarea>
         <button type="submit"> Pedir </button>
       </form>
     `
