86f6199f54b35792cb2b5caad685462d340a6b8f
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-06-27 22:44:29 +0200

    errores de validación basados en datos

--- src/app/order-form.directive.js
+++ src/app/order-form.directive.js
@@ -8,28 +8,38 @@ function orderForm() {
     restrict: 'E',
     controller: 'OrderFormController as form',
     template: `
-      <pre>{{ orderForm | json }}</pre>
-      <pre>{{ form.data | json }}</pre>
       <form name="orderForm" novalidate ng-submit="form.onSubmit();">
         <input name="name"
                type='text'
                placeholder='Nombre'
                ng-model="form.data.name"
                required=''>
+        <div ng-show="orderForm.name.$error.required && orderForm.name.$touched">
+          El nombre es obligatrio!
+        </div>
         <input name="email"
                type='email'
                placeholder='Email'
                ng-model="form.data.email"
                required=''>
+       <div ng-show="orderForm.email.$error.required && orderForm.email.$touched">
+         El email es obligatrio!
+       </div>
+       <div ng-show="orderForm.email.$error.email && orderForm.email.$touched">
+         El email no es correcto!
+       </div>
         <input name='location'
                placeholder='Ciudad'
                ng-model="form.data.location"
                required=''>
+         <div ng-show="orderForm.location.$error.required && orderForm.location.$touched">
+           La ciudad es obligatria!
+         </div>
         <select name="orderChoice"
                 ng-model='form.data.product'
                 ng-options='product.label for product in form.products'
                 required=''>
-          <option>Tu ciudad</option>
+          <option value="">Selecciona...  </option>
         </select>
         <textarea name="comments" placeholder='Tu mensaje (opcional)' ng-model="form.data.comments"></textarea>
         <button type="submit"
